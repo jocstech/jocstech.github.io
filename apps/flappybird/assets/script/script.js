d6b86257c012b90bf307b94211782ccbd62bdada
jocstech <jocstech@gmail.com>
2017-03-24 00:59:01 -0400

    added web apps demo icon and link-Yulong

--- apps/flappybird/assets/script/script.js
+++ apps/flappybird/assets/script/script.js
@@ -1,11 +1,11 @@
 debugMode = false;
 
+
 // system:
 var doc,
     win,
-    canvas;
-
-var winW,
+    canvas,
+    winW,
     winH;
 
 // webgl:
@@ -14,15 +14,24 @@ var canvas,
     ctx;
 
 var id;
-var timerId;
-var count=0;
+var count = 0;
 
 // game styles
 
-var color = ['Blue','BlueViolet','Coral','DarkGreen','Gold','LightSeaGreen','Lime','YellowGreen','Turquoise','SteelBlue','Salmon','PowderBlue'];
+var cloudTypes = ['assets/images/cloud.svg',
+                  'assets/images/cloud.svg',
+                  'assets/images/cloud.svg'];
+
+var color = ['#E08031',
+             '#C7CE82',
+             '#199475',
+             '#0B6E48',
+             '#044D22'];
 
 var bgm;
 var sounde;
+
+var canvasBG;
 
 // characters:
 
@@ -37,45 +46,56 @@ var bird,
 
 
 var gap;
+var obsW;
 
 // logics:
 
 var dead;
+var obss = [];
+var clouds = [];
 
-var obs1 = {x:0,height:0,color1:'white',color2:'white'};
-var obs2 = {x:0,height:0,color1:'white',color2:'white'};
-var obs3 = {x:0,height:0,color1:'white',color2:'white'};
-var obs4 = {x:0,height:0,color1:'white',color2:'white'};
+var firstTime;
 
 var mvSpeed;
+var oldSpeed;
 var fps;
+var lastCalledTime;
 var time;
 
 // player info
 
 var username;
 var score;
+var level;
+var best=0;
+var got;
 
-var got1 = {x:window.innerWidth/4-80,y:-20};
-var got2 = {x:window.innerWidth/4-40,y:-20};
+// game rules
+
+var gapSetting,
+    gapStep,
+    scoreSetting,
+    scoreStep;
 
 // Main Function:
 (function(){
+
     //buildGame();
     homePage();
 })();
 
 function homePage(){
+    
+    firstTime = true;
+    
     push('body','<div id="home"><div>');
     push('#home','<h1>Welcome to Yulong\'s Flappy Bird</h1>');
     push('#home','<div id="prom"><div>')
+    push('#prom','<img id="logo" src="assets/images/bird002.svg" alt="">');
     push('#prom','<h1>Type Your Name:</h1>');
-    push('#prom','<form id="form" method="#" action="#"></form>');
+    push('#prom','<div id="form"></div>');
     push('#form','<input id="name" type="text"><br>');
-    push('#form','<input id="enterbtn" value="Enter Game" type="submit" onclick="buildGame()">');
-     push('#prom','<img id="logo" src="assets/images/bird002.svg" alt="">');
-    
-    
+    push('#form','<button id="enterbtn">Enter Game</button>'); 
     // properties
     $('#home').css('width','100%').css('height','100%').css('text-align','center');
     $('#prom').css('width','50%')
@@ -85,14 +105,18 @@ function homePage(){
               .css('text-align','center')
               .css('border','1px solid black');
     $('#prom h1').css('color','white');
-    $('#enterbtn').css('width','130').css('height','200');
-    $('#logo').css('width','50%').css('height','70%');
-    
+    $('#logo').css('max-width','30%').css('height','30%');
+    $('#enterbtn').click(buildGame);
+}
+
+
+function getUsername(){
+    username = $('#name').val();
+    $('#home').remove();
 }
 
 function buildGame(){
-    username = $('#name').val();
-    $('#home').remove();
+    if(firstTime) getUsername();
     init();
     DivInit();
     setup();
@@ -102,21 +126,30 @@ function buildGame(){
     startAnimation();
 }
 
+function clearUp(){
+    firstTime = false;
+    destroyEventListener();
+}
 
 // Functions:
 function init(){
+    
     doc = document;
     win = window;
-    winW = window.innerWidth;
-    winH = window.innerHeight;
+    winW = window.innerWidth*2;
+    winH = window.innerHeight*2;
     
+    
+    canvasBG = '#EAF2F8'; 
     bgm = $('#bgm')[0];
+    bgm.currentTime = 0;
     sounde = $('#sounde')[0];
+    sounde.currentTime = 0;
     
     bird = new Image();
     bird.src='assets/images/bird002.svg';
     size = 20;
-    gravity = 0.09;
+    gravity = 0.15;
     
     // initial speed
     V0 = 0;
@@ -125,29 +158,89 @@ function init(){
     angle = 0;
     dead = false;
     
-    // obs
-    gap = 100;
-    mvSpeed = 0.5;
-    fps = 1;
-    obs1.x = winW/2;
-    obs1.height = getRandomInt(2,winH/3);
+    // game rules
+    lastCalledTime = Date.now() ;
+    gapSetting = 400;
+    gapStep = 20;
+    scoreSetting = 30;
+    scoreStep = 30;
     
-    obs2.x = obs1.x+winW/4;
-    obs2.height = getRandomInt(2,winH/3);
+    // obs
+    gap = gapSetting;
+    obsW = 40;
+    mvSpeed = 1;
+    oldSpeed = 1;
+    fps = 0;
+    
+    createClouds();
+    buildObstacles();
+    
+    if((username.length<1)&&((username=='')||(username==null)||(username==undefined))) username = "Flappy Bird";
     
     score = 0;
+    level = 1;
+    
+    got = {x:winW/2,y:-20};
+    
+}
+
+function createClouds(){
+    // clouds
+    var num = Math.round(winW/300);
+    var oldPos = 0;
+    var cloud;
+    for(var n  = 0 ; n < num ; n++){
+        var len = cloudTypes.length-1;
+        var index = getRandomInt(0,len);
+        var size = getRandomInt(50,500);
+        var speed = getRandomInt(1,6);
+        if(n==0){
+            cloud = {x:winW,y:getRandomInt(-size,winH/2),size:size,cloudType:index,speed:speed};
+        } else {
+            cloud = {x:oldPos+winW/num,y:getRandomInt(-size,winH/2),size:size,cloudType:index,speed:speed};
+        }
+        oldPos = cloud.x;
+        clouds.push(cloud);
+    }
+}
+
+
+function buildObstacles(){    
+    obss = generateObstacle(Math.round(winW/400));
+}
+
+
+function generateObstacle(num){
+    
+    var newObss=[];
+    var oldPos = 0;
+    var obs;
+    for(var n = 0 ; n < num ; n++){
+        var clen = color.length-1;
+        var cindex = getRandomInt(0,clen);
+        if(n==0){
+            obs = {x:winW+obsW,height:getRandomInt(2,winH/3),color1:color[cindex],color2:color[clen-cindex]};
+        } else {
+            obs = {x:oldPos+winW/num,height:getRandomInt(2,winH/3),color1:color[cindex],color2:color[clen-cindex]};
+        }
+        oldPos = obs.x;
+        newObss.push(obs);
+    }
+    return newObss;
 }
 
 function update(){  
-    winW = window.innerWidth;
-    winH = window.innerHeight;
+    retinaSupport(canvas);
+    gameRulesCheck();
     collisionCheck();
     draw();
 }
 
 function DivInit(){
     //push('body','<h1>New Canvas</h1>');
-    push('body','<canvas id="canvas"></canvas>')    
+    $('#canvas').remove();
+    push('body','<canvas id="canvas"></canvas>');
+    push('body','<input id="btnPlayAgain" type="button" style="z-index:2; position:absolute; top:-50px; left:'+(winW/2)+'px" value="Play Again"/>');
 }
 
 function setup(){
@@ -158,71 +251,92 @@ function webglInit(){
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     //Retina sup
-    ctx.scale(2,2)
+    retinaSupport(canvas);
 }
 
 function drawScene(){
-    winW = window.innerWidth;
-    winH = window.innerHeight;
+    winW = window.innerWidth*2;
+    winH = window.innerHeight*2;
+    ctx.save();
+    ctx.fillStyle = canvasBG;
     ctx.fillRect(0,0,winW,winH);
+    ctx.restore();
+    drawClouds();
 }
 
  
 function draw(){
-    drawScene();
+    drawScene(); 
     drawObs();
     drawImgAngle(bx,by,size,angle,bird);
     screenCheck();
 }
 
 function controlUpdate(){
-    V0 += -9;
-    angle= 0.6;
+    V0 -= 12;
+    angle= 1;
 }
 
 
+function destroyEventListener(){
+     $(window).off();
+}
+
 function EventListener(){
     $(window).on('resize',function(){
-        //update();
+        buildObstacles();
+        got.x = winW/2;
+        update();
     });
     
-    $(window).on('tap',function(){
-        controlUpdate();
-    });
-    
-    $(window).click(function(e){
-        controlUpdate();
-    });
-    
+    $(window).on('tap',controlUpdate);
+    $(window).click(controlUpdate);
+
     $(window).keypress(function(e){
         var key = e.keyCode ? e.keyCode : e.which;
         // spacebar
         if(key == 32) {
             controlUpdate();
         }
+        // right
+        if (key == 68 || key == 39 || key == 100) {
+            oldSpeed = mvSpeed;
+        }
     });
     
     
     $(window).keydown(function(e){
         var key = e.keyCode ? e.keyCode : e.which;
-        
+        // right
         if (key == 68 || key == 39 || key == 100) {
-            mvSpeed = 1;
+            if(mvSpeed<5)
+                mvSpeed += 0.1;
+        }
+        if (key == 65 || key == 37) {
+            if(mvSpeed>1)
+                mvSpeed -= 0.1;
         }
     });
     
     $(window).keyup(function(e){
-         var key = e.keyCode ? e.keyCode : e.which;
-         if(key == 32) {
-             //V0 = 0.5;
-        }
-        
+        var key = e.keyCode ? e.keyCode : e.which;        
         if (key == 68 || key == 39 || key == 100){
-            mvSpeed = 0.5;
+            //
         }
         
     });
     
+}
+
+function gameRulesCheck(){
+    if(score>scoreSetting && gap>120){
+        gap = gapSetting;
+        gapSetting -= gapStep;
+        scoreSetting += scoreStep;
+    }
+    if(score>best) best = score;
+    level = 1+Math.floor(score/scoreStep);
+    mvSpeed = mvSpeed+level*0.0001;
 }
 
 function screenCheck(){
@@ -231,16 +345,24 @@ function screenCheck(){
     // when game over:
     if(dead){
         drawGameOver();
-        console.log("Player Dead!");
     }
 }
 
 function screenCheckAnimation(){
-    if(got1.y<(window.innerHeight/4))
-        got1.y+=0.5;
-    if(got2.y<(window.innerHeight/4+20))
-        got2.y+=0.2;
+    if(got.y<(window.innerHeight))
+        got.y+=1;
+    else{
+        clearInterval(id);
+
+        $('#btnPlayAgain').click(function(){
+            $(this).off();
+            $(this).remove();
+            clearUp();
+            buildGame();
+        });
+    }
     update();
+
 }
 
 function startAnimation(){
@@ -249,10 +371,7 @@ function startAnimation(){
 }
 
 
-function frame() {
-    
-    time++;
-    
+function frame() {    
     if (dead) {
         
         bgm.pause();
@@ -267,11 +386,11 @@ function frame() {
         
         moving();
         
-        if(size<40){
+        if(size<100){
             size+=0.5;
-            bx = (winW/8)-(size/2);
+            bx = (winW/4)-(size/2);
         }
-        
+        cloudsAnimation();
         obsAnimation();
         count++;
         update();
@@ -279,10 +398,10 @@ function frame() {
 }
 
 function moving(){
-    if(by<winH/2-size+10){
+    if(by<winH-size+10){
        V0+=gravity;
-       V0*=0.9;
-       by+=V0*fps;
+       V0*=0.95;
+       by+=V0;
     } else {
         dead = true;
     }
@@ -290,88 +409,126 @@ function moving(){
 
 function obsAnimation() {
     if (!dead) {
-        if (obs1.x > -15) {
-            obs1.x -= mvSpeed * fps;
-            if(Math.floor(obs1.x/bx)==1){
+        for( var n = 0 ; n < obss.length ; n ++){
+            if (obss[n].x > -obsW) {
+            obss[n].x -= mvSpeed;
+            if(Math.floor(obss[n].x)==Math.floor(bx+obsW)){
                 sounde.currentTime = 0;
                 sounde.play();
                 score++;
             }
         } else {
-            obs1.height = getRandomInt(10, winH / 3);
-            var num = getRandomInt(0,11);
-            obs1.color1 = color[num];
-            obs1.color2 = color[11-num];
-            obs1.x = winW / 2;
+            obss[n].height = getRandomInt(10, winH*2 / 3);
+            var clen = color.length-1;
+            var num = getRandomInt(0,clen);
+            obss[n].color1 = color[num];
+            obss[n].color2 = color[clen-num];
+            obss[n].x = winW ;
         }
-        if (obs2.x > -15) {
-            obs2.x -= mvSpeed * fps;
-            if(Math.floor(obs2.x/bx)==1) {
-                sounde.currentTime = 0;
-                sounde.play();
-                score++;
-            }
-        } else {
-            obs2.height = getRandomInt(10, winH / 3);
-            var num = getRandomInt(0,11);
-            obs2.color1 = color[num];
-            obs2.color2 = color[11-num];
-            obs2.x = winW / 2;
         }
     }
 }
+
+function cloudsAnimation(){
+     for( var n = 0 ; n < clouds.length ; n ++){
+            if (clouds[n].x > -clouds[n].size) {
+                clouds[n].x -= clouds[n].speed;
+        } else {
+            clouds[n].speed = getRandomInt(1,6);
+            clouds[n].x = winW ;
+            clouds[n].y = getRandomInt(-10, winH*2 / 4);
+        }
+    }
+}
+
+function getFPS(){
+  var delta = (Date.now() - lastCalledTime)/1000;
+  lastCalledTime = Date.now();
+  fps = 1/delta;
+    
+  return Math.floor(fps);
+}
+
+
 function drawGameOver(){
-    ctx.save()
+    ctx.save();
+    fillRoundRect(got.x-200+10, got.y-150+10, 400 ,300 , 20 , 'rgba(1,1,1,0.1)');
+    fillRoundRect(got.x-200, got.y-150, 400 ,300 , 20 , 'rgba(255,255,255,0.8)');
+    ctx.textAlign="center"; 
+    ctx.font = "60px Arial";
+    ctx.fillStyle = "black";
+    ctx.fillText("Game Over!", got.x,got.y-30);
     ctx.font = "30px Arial";
-    ctx.fillStyle = "white";
-    ctx.fillText("Game Over!", got1.x,got1.y);
-    ctx.font = "15px Arial";
-    ctx.fillText("Best Score: "+score, got2.x,got2.y);
+    ctx.fillText("Best Score: "+best, got.x,got.y+30);
     ctx.restore();
+    $('#btnPlayAgain').css('top',(got.y/2+30)+'px').css('left',(got.x/2-45)+'px');
 }
 
 function drawScore(){
-    ctx.save()
-    ctx.font = "10px Arial";
+    ctx.save();
+    
+    // Copyright
+    ctx.fillStyle = "black";
+    ctx.font = "25px Arial";
+    ctx.fillText("Developer: Yulong Fang",20,winH - 45);
+    ctx.font = "20px Arial";
+    ctx.fillText("Copyright@Jocstech "+new Date().getFullYear(),20,winH - 20);
+    
+    fillRoundRect(20,20,190,135,10,'rgba(155,155,155,0.5)');
+    ctx.font = "25px Arial";
     ctx.fillStyle = "gray";
-    ctx.fillText("Score: "+score,21,21);
+    ctx.fillText("Level:  "+level,42,92);
     ctx.fillStyle = "white";
-    ctx.fillText("Score: "+score,20,20);
-    ctx.fillText(username,winW/4-10,20);
+    ctx.fillText("Level:  "+level,40,90);
+    ctx.fillStyle = "gray";
+    ctx.fillText("Score: "+score,42,117);
+    ctx.fillStyle = "white";
+    ctx.fillText("Score: "+score,40,115);
+    ctx.fillStyle = "gray";
+    ctx.fillText("Best:   "+best,42,142);
+    ctx.fillStyle = "white";
+    ctx.fillText("Best:   "+best,40,140);
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "gray";
+    ctx.fillText(username,40,59);
+    ctx.fillStyle = "white";
+    ctx.fillText(username,42,57);
+    fillRoundRect(winW-160,20,150,50,10,'rgba(0,0,0,0.6)');
+    ctx.fillText('FPS:'+getFPS(),winW-140,55);
+    ctx.textAlign="center"; 
+    ctx.font = "80px Arial";
+    ctx.fillStyle = "gray";
+    ctx.fillText(Math.floor(count/100)+'m',winW/2,winH-40);
     ctx.restore();
 }
 
+function drawClouds(){
+    for(var n = 0 ; n < clouds.length ; n++){
+        drawImg(clouds[n].x,clouds[n].y,clouds[n].size,cloudTypes[clouds[n].cloudType]);
+    }
+}
+
 function drawObs(){    
-    fillRect(obs1.x,0,15,obs1.height,5,obs1.color1);
-    fillRect(obs1.x,obs1.height+gap,15,winH/2-obs1.height-gap,5,obs1.color2);
+    for(var n = 0 ; n < obss.length ; n++){
+        fillRect(obss[n].x,0,obsW,obss[n].height,5,obss[n].color1);
+        fillRect(obss[n].x,obss[n].height+gap,obsW,winH-obss[n].height-gap,5,obss[n].color2);
     
+        fillCircle(obss[n].x+obsW/2,obss[n].height-13,obss[n].color1);
+        fillCircle(obss[n].x+obsW/2,obss[n].height+gap+13,obss[n].color2); 
     
-    
-    fillCircle(obs1.x+15/2,obs1.height-13,obs1.color1);
-    fillCircle(obs1.x+15/2,obs1.height+gap+13,obs1.color2);
-    
-    fillRect(obs2.x,0,15,obs2.height,5,obs2.color1);
-    fillRect(obs2.x,obs2.height+gap,15,winH/2-obs2.height-gap,5,obs2.color2);
-    
-    fillCircle(obs2.x+15/2,obs2.height-13,obs2.color1);
-    fillCircle(obs2.x+15/2,obs2.height+gap+13,obs2.color2);
-    
-    if(obs1.x+15/2>bx)
-        drawFuzzyLines(obs1.x+15/2,obs1.height-13,obs1.x+15/2,obs1.height+gap+13);
-    if(obs2.x+15/2>bx)
-    drawFuzzyLines(obs2.x+15/2,obs2.height-13,obs2.x+15/2,obs2.height+gap+13);
-    
-    
+        if(obss[n].x+obsW/2>bx+100)
+            drawFuzzyLines(obss[n].x+obsW/2,obss[n].height-13,obss[n].x+obsW/2,obss[n].height+gap+13);
+    }
 }
 
 function drawFuzzyLines(x1,y1,x2,y2){
     
-    var wide = 20;
+    var wide = 25;
     var p1x,p1y,p2x,p2y;
     
     ctx.save;
-    ctx.strokeStyle = 'PaleGoldenRod';
-    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'chartreuse';
+    ctx.lineWidth = 5;
     ctx.beginPath();
     ctx.moveTo(x1, y1);
     for(var i = 0 ; i <3 ; i++){
@@ -384,7 +541,7 @@ function drawFuzzyLines(x1,y1,x2,y2){
     ctx.quadraticCurveTo(p2x,p2y,x2,y2);
     ctx.closePath;
     ctx.stroke();
-    ctx.restore;
+    ctx.restore();
     
     
     
@@ -420,8 +577,25 @@ function fillRect(x,y,width,height,lineWidth,color){
     ctx.restore();
 }
 
+function fillRoundRect(x, y, width, height, radius, color) {
+    if (width < 2 * radius) radius = width / 2;
+    if (height < 2 * radius) radius = height / 2;
+    ctx.save();
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.moveTo(x + radius, y);
+    ctx.arcTo(x + width, y, x + width, y + height, radius);
+    ctx.arcTo(x + width, y + height, x, y + height, radius);
+    ctx.arcTo(x, y + height, x, y, radius);
+    ctx.arcTo(x, y, x + width, y, radius);
+    ctx.closePath();
+    ctx.fill();
+    ctx.restore();
+}
+
+
 function fillCircle(centerX,centerY,color){
-    var radius = 15;
+    var radius = obsW;
     ctx.save();
     ctx.beginPath();
     ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
@@ -430,11 +604,11 @@ function fillCircle(centerX,centerY,color){
     ctx.fill();
     ctx.restore();
     
-    radius = 10;
+    radius = obsW - 6;
     ctx.save();
     ctx.beginPath();
     ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = canvasBG;
     ctx.closePath;
     ctx.fill();
     ctx.restore();
@@ -452,21 +626,16 @@ function drawLine(fromx, fromy, tox, toy){
     ctx.moveTo(fromx, fromy);
     ctx.lineTo(tox, toy);
     ctx.stroke();
-    ctx.restore;
+    ctx.restore();
 }
 
 
-function collisionCheck(){
-    // bx , by
-    //obs1.x obs1.y
-    //obs2.x obs2.y
-    
-    if((bx+size)>obs1.x && (bx+size)<obs1.x+15){
-        if(((by)< obs1.height)||((by+size/2) > obs1.height+gap)) dead = true;
-    }
-    
-    if((bx+size)>obs2.x && (bx+size)<obs2.x+15){
-        if(((by)< obs2.height)||((by+size/2) > obs2.height+gap)) dead = true;
+function collisionCheck(){    
+    for(var n = 0 ; n < obss.length ; n++){
+        if((bx+size)>obss[n].x && (bx+size)<obss[n].x+obsW){
+            if(((by)< obss[n].height)||((by+size) > obss[n].height+gap))
+                dead = true;
+        }
     }
 }
 
@@ -483,4 +652,19 @@ function push(div,elm){
 
 function debug(msg){
     console.log(msg);
+}
+
+
+function retinaSupport(canvas) {
+    if (window.devicePixelRatio) {
+        var realToCSSPixels = window.devicePixelRatio;
+        var displayWidth = Math.floor(canvas.clientWidth * realToCSSPixels);
+        var displayHeight = Math.floor(canvas.clientHeight * realToCSSPixels);
+        // Check if the canvas is not the same size.
+        if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
+            // Make the canvas the same size
+            canvas.width = displayWidth;
+            canvas.height = displayHeight;
+        }
+    }
 }
